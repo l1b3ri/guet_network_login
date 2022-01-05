4a5fad099b4268acfc4fc3ed53827e84572e5648
l1b3ri <244004036@qq.com>
2022-01-05 19:35:01 +0800

    Update 每天启动并添加到开机自启动.bat

--- 每天启动并添加到开机自启动.bat
+++ 每天启动并添加到开机自启动.bat
@@ -1,5 +1,5 @@
 @echo off
-schtasks /Create /TN "guet_network_login" /TR login.py /RU %username% /SC DAILY
+schtasks /Create /TN "guet_network_login" /TR login.py /RU %username% /SC DAILY /MO 1
 echo success
 echo start login.py > "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\guet_login.bat"
 
